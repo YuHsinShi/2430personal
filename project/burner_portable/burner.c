3c4e9badff3151837c178a484a780715a6630ffe
lawrence.shi <lawrence.shi@ite.com.tw>
2020-09-30 00:16:58 +0800

    1st ui to 1st version to remove UI and uncessary part from toolman project

---
+++ project/burner_portable/burner.c
@@ -0,0 +1,400 @@
+﻿#include "ite/itu.h"
+#include "project.h"
+#include "scene.h"
+
+#include "ssp/mmp_spi.h"
+
+#ifndef WIN32
+static pthread_t burnTask;
+
+#define BURNER_STATUS_IDLE	0
+#define BURNER_STATUS_ROM_NOT_EXISTED	-1
+#define BURNER_STATUS_ROM_NOT_EXISTED	-1
+
+void set_io_high(unsigned int pin)
+{
+	ithGpioSetOut(pin);
+	ithGpioSetMode(pin, ITH_GPIO_MODE0);
+	ithGpioSet(pin);
+
+}
+void set_io_low(unsigned int pin)
+{
+	ithGpioSetOut(pin);
+	ithGpioSetMode(pin, ITH_GPIO_MODE0);
+	ithGpioClear(pin);
+
+}
+
+void switch_to_channel(int channel)
+{
+switch(channel)
+{
+	case 1:
+		set_io_low(48);
+		set_io_low(49);
+		set_io_low(50);
+		break;
+	case 2:
+		set_io_low(48);
+		set_io_low(49);
+		set_io_high(50);
+
+		break;
+	case 3:
+		set_io_low(48);
+		set_io_high(49);
+		set_io_low(50);
+		break;
+	case 4:
+		set_io_low(48);
+		set_io_high(49);
+		set_io_high(50);
+		break;
+	case 5:
+		
+		set_io_high(48);
+		set_io_high(49);
+		set_io_low(50);
+		break;
+	default:
+		printf("not defined ");
+		break;
+		
+
+}
+
+}
+
+
+
+void write_slave_register(uint32_t addr, uint32_t data)
+{
+	uint8_t cmd[9] = { 0x0 };
+	uint8_t indata[9] = { 0 };
+	uint32_t *tmp_u32 = 0;
+	uint32_t addr_endian;
+	uint32_t data_endian;
+
+	cmd[0] = 0x04;
+	//addr_endian= BLEndianUint32(addr);
+	addr_endian = addr;
+	tmp_u32 = (uint32_t*)&cmd[1];
+	memcpy(tmp_u32, &addr_endian, 4);
+
+	//data_endian= BLEndianUint32(data);
+	data_endian = data;
+
+	tmp_u32 = (uint32_t*)&cmd[5];
+
+	memcpy(tmp_u32, &data_endian, 4);
+
+
+	mmpSpiPioWrite(SPI_1, cmd, 9, 0, 0, 0);
+
+
+}
+
+
+void read_slave_register(uint32_t addr, uint8_t* indata)
+{
+	uint8_t cmd[9] = { 0x0 };
+
+	uint32_t *cmd_reg_addr = 0;
+	uint32_t addr_endian;
+
+	cmd[0] = 0x45;
+	//addr_endian= BLEndianUint32(addr);
+	addr_endian = addr;
+	cmd_reg_addr = (uint32_t*)&cmd[1];
+	memcpy(cmd_reg_addr, &addr_endian, 4);
+
+	mmpSpiPioRead(SPI_1, cmd, 9, indata, 4, 4);
+	/*
+	printf("read_slave_register addr 0x%x\n",addr);
+	for(int i=0;i<9;i++)
+	printf("0x%x ",indata[i]);
+	printf("\n");
+	*/
+
+}
+
+
+int get_ite_chip_id()
+{
+	uint32_t chip_id;
+
+	uint8_t indata[16]={0};
+	read_slave_register(0xd8000004,indata);//packet3
+	for(int i=0;i<9;i++)
+	printf("0x%x ",indata[i]);	
+		printf("\n");
+
+	if( (indata[3]==0x9) && (indata[2]==0x60) )
+	{
+		printf("chip id is %x%x",indata[2],indata[3]);
+		return 1;
+	}
+	else if ( (indata[3]==0x9 && indata[2]==0x70))
+		return 2;
+	else
+		return -1;
+
+}
+
+int set_bypass_mode_970()
+{
+	printf("set_bypass_mode \n");
+
+	write_slave_register(0xd1000230,0x80058006);	
+	write_slave_register(0xd1000234,0x80088007);	
+	write_slave_register(0xd1000204,0x02000000);	
+
+	return 0;
+}
+
+int set_bypass_mode_960()
+{
+	printf("set_bypass_mode \n");
+
+	write_slave_register(0xd1000230,0x800a8005);	
+	write_slave_register(0xd1000234,0x80078006);	
+	write_slave_register(0xd1000204,0x02000000);	
+
+	return 0;
+}
+
+
+void writing_progressing(uint32_t i)
+{
+	
+#define ROM_SIZE_WRITE_PIECE  2*1024*1024 //1024*1024
+
+
+	uint8_t* rom_content=NULL;
+	uint8_t* rom_read_content=NULL;
+
+	char filepath_tmp[64];
+	uint32_t read_size;
+	uint32_t rom_size_total;
+	FILE* fp;
+	uint32_t addr;
+	float percent;
+
+			snprintf(filepath_tmp,64,"E:/%d.rom",i);
+			fp = fopen(filepath_tmp, "r");
+			if (NULL == fp)
+			{
+				printf("fp can not open %s\n",filepath_tmp);
+				return;
+			}
+
+			fseek(fp, 0, SEEK_END);
+			rom_size_total = ftell(fp);
+			fseek(fp, 0, SEEK_SET);		
+
+
+			uint32_t tick;
+			//tick=SDL_GetTicks();
+			printf("Nor2nd_Write start \n");
+			tick = xTaskGetTickCount();
+
+
+
+
+			rom_content = malloc(ROM_SIZE_WRITE_PIECE);
+			rom_read_content = malloc(ROM_SIZE_WRITE_PIECE);
+			
+			if( (NULL==rom_content)||(NULL==rom_read_content) )
+			{
+				printf("rom_content ERROR \n");
+				return;
+			}
+
+			addr=0x0;//write from 0 address
+			while(1)
+			{
+				read_size=fread(rom_content,1,ROM_SIZE_WRITE_PIECE,fp);
+				if(0==read_size)
+					break;
+
+				while(1)
+				{
+				
+					Nor2ndErase(addr,ROM_SIZE_WRITE_PIECE);
+					printf("Nor2nd_Write addr=0x%x,size=0x%x \n",addr,read_size);
+					Nor2nd_Write(addr,rom_content,read_size);
+					printf("Nor2nd_Read addr=0x%x \n",addr);
+					
+					Nor2nd_Read(addr,rom_read_content,read_size);
+					printf("Nor2nd_Compare addr=0x%x \n",addr);
+					if(0 != memcmp(rom_content,rom_read_content,read_size ) )
+					{
+						printf("WRITE ERROR in address 0x%x \n",addr);
+						printf("rom_content 0x%x,0x%x,0x%x,0x%x \n",rom_content[0],rom_content[1],rom_content[2],rom_content[3]);
+						printf("rom_read_content 0x%x,0x%x,0x%x,0x%x \n",rom_read_content[0],rom_read_content[1],rom_read_content[2],rom_read_content[3]);
+						return;
+					}
+					
+					else
+					{
+						break;
+					}
+					
+				}
+				addr+=read_size;
+				percent= (float)((float)addr/(float)rom_size_total);
+					printf("progress %f percent \n",percent*100);
+					
+				//	printf("flashing %d \n",flashing);
+				//	led_flash_set_level(flashing);
+				//	flashing=flashing-10;
+					BurnerOnTimer_ui_set(0,(int)(percent*100)); //update current bar
+					BurnerOnTimer_ui_set(i,(int)(percent*100));  //update rest bar
+
+			}	
+
+		printf("Nor2nd_Write finished elapsed %d \n",elased_tick(tick));
+
+		free(rom_content);
+		free(rom_read_content);
+
+}
+
+//ret 1: nor init OK
+//ret -1: can not found slave device or not supported nor
+int burner_check_mode()
+{
+
+	//use fastest mode to check slave
+		SPI_CLK_LAB my_clk;
+	int ret;
+
+	
+	for(my_clk=SPI_CLK_20M;my_clk>=SPI_CLK_5M;my_clk-- )
+	{
+		mmpSpiInitialize(SPI_1, SPI_OP_MASTR, CPO_0_CPH_0, my_clk);
+		ret =	get_ite_chip_id();
+		//check if slave is iTE chip // change to nor mode if in iTE mode
+		if(ret >0)
+		{	
+			if(1== ret) //960 series
+			{
+				set_bypass_mode_960();
+				if(1==Nor2nd_Init())
+				{
+				  return 0x960;//
+				}
+			}
+			else if(2== ret) //960 series
+			{
+
+				set_bypass_mode_970();
+				if(1==Nor2nd_Init())
+				{
+					return 0x970;//
+
+				}
+
+			}
+			else
+			{
+
+			}
+		}
+		
+
+		if(1==Nor2nd_Init())	//check if slave is nor chip
+		{
+			return 1;
+
+		}
+		else
+		{
+				//do nothing check 
+		}
+
+		mmpSpiTerminate(SPI_1);
+
+	}
+
+return -1;
+}
+
+
+static void* burnningTask(void* arg)
+{
+
+	if(1 == burner_check_mode())
+	{
+		//ic found 
+	   writing_progressing(1);
+		
+	   mmpSpiTerminate(SPI_1);
+	}
+	else
+	{
+		//fail connection fail, check cable
+	}
+	
+}
+
+void burn_writer_stop()
+{
+   pthread_join(burnTask, NULL);
+}
+
+void burn_writer_start()
+{
+	printf("burn_writer_start\n");
+	    pthread_create(&burnTask, NULL, burnningTask, NULL);
+}
+
+
+#endif
+
+void burn_switching_task(void* arg)
+{
+		uint32_t i;
+		int ret;
+#ifndef WIN32
+
+		
+		while(1)
+		{
+			for(i=1;i<=5;i++)
+			{
+	
+				switch_to_channel(i);
+
+				BurnerOnTimer_ui_set(i,0); //update current bar
+
+				if(1 == burner_check_mode())
+				{
+					//ic found 
+				   writing_progressing(i);
+				
+				   mmpSpiTerminate(SPI_1);
+				}
+				else
+				{
+					//fail connection fail, check cable
+				}
+				sleep(1);
+			}
+	
+		}
+#endif
+}
+
+
+void burn_switching_start()
+{
+	static pthread_t switchTask;
+
+	printf("burn_writer_start\n");
+	pthread_create(&switchTask, NULL, burn_switching_task, NULL);
+}
+
+
+
---
+++ project/burner_portable/external.c
@@ -0,0 +1,661 @@
+#include <sys/ioctl.h>
+#include <assert.h>
+#include <mqueue.h>
+#include <string.h>
+#include <unistd.h>
+#include "ite/itp.h"
+#include "project.h"
+#include "scene.h"
+
+#define EXT_MAX_QUEUE_SIZE      8
+#define MAX_OUTDATA_SIZE        64
+
+#define UartHeader     		0xFF
+#define UartTotalLength     16
+#define UartPayloadLength   13
+
+// Example for UART payload reference
+#define UartPayload0   		0x00
+#define UartPayload1   		0x01
+#define UartPayload2   		0x02
+#define UartPayload3   		0x03
+#define UartPayload4   		0x04
+#define UartPayload5   		0x05
+#define UartPayload6   		0x06
+#define UartPayload7   		0x07
+#define UartPayload8   		0x08
+#define UartPayload9   		0x09
+#define UartPayload10  		0x0A
+#define UartPayload11  		0x0B
+#define UartPayload12  		0x0C
+#define UartCheckSum		0x4E  // CheckSum = payload0 + payload1 + ...... payload12
+
+#ifdef _WIN32
+#define WIN32_COM           5
+#endif
+
+typedef enum GET_UART_COMMAND_STATE_TAG
+{
+    GET_HEADER,
+    GET_LENGTH,
+    GET_PAYLOAD,
+    GET_CHECKSUM,
+} GET_UART_COMMAND_STATE;
+
+static mqd_t extInQueue = -1;
+static mqd_t extOutQueue = -1;
+static pthread_t extTask;
+static pthread_t WrTask;
+
+static volatile bool extQuit;
+
+static volatile bool WritingQuit;
+
+static unsigned char inDataBuf[EXTERNAL_BUFFER_SIZE];
+static unsigned char cmdBuf[EXTERNAL_BUFFER_SIZE];
+static unsigned int cmdPos = 0;
+static GET_UART_COMMAND_STATE gState = GET_HEADER;
+static unsigned int payloadCount = 0;
+static unsigned int checkSum = 0;
+
+
+#define WRITING_DEBUG 	1
+
+#define HEADER_PACKET_SIZE		4  //4 BYTE
+#define HEADER_UART_NUMBER		1  //1 BYTE
+#define HEADER_CMD		1  //4 BYTE
+
+
+
+#define HEADER_SHIFT 		(HEADER_UART_NUMBER+HEADER_PACKET_SIZE+HEADER_CMD)
+
+#define LOG_TIMESTAMP_LEN		12 //10//[12:34:56] //10 content with /0
+#define HEADER_RESERVED 		LOG_TIMESTAMP_LEN+HEADER_SHIFT
+
+
+#if WRITING_DEBUG
+#define DBG(fmt, ...)		fprintf(stderr, "[DBG] "fmt, ##__VA_ARGS__);
+#else
+#define DBG(fmt, ...)
+#endif
+#define ERR(fmt, ...)		fprintf(stderr, "[DBG] "fmt, ##__VA_ARGS__);
+
+
+//check if usb existed
+//check  space available
+//read uart and write uart
+//timeout buffer full
+static FILE* wrhandle;
+
+
+#define LOG_WRITER_STATUS_IDLE 		0
+#define LOG_WRITER_STATUS_BOOTING 	1
+#define LOG_WRITER_STATUS_POWER_OFF 	2
+#define LOG_WRITER_STATUS_END			3
+
+
+#define LOG_WRITER_CMD_NORMAL 			0
+#define LOG_WRITER_CMD_FAIL_BOOT 		1
+#define LOG_WRITER_CMD_OK_BOOT 			2
+
+#define LOG_WRITER_MODE_NORMAL		0
+#define LOG_WRITER_MODE_POWER_ONOFF		1
+
+
+struct logwrite_t {
+    ITPDeviceType itp_uart_index;
+	int mode;				//if 0: capture mode, 1; booting mode
+	char folder_path[64];	//path for writing 
+	char file_name;			//the name of latest log
+	unsigned int num; 				//num of the log 
+	FILE* handle; 			//handle NULL not open or closed
+	unsigned int byte_counter; 		//counting byte to END the opened file
+	unsigned int last_time; 			//last time to generate system time string
+
+	
+	unsigned int alive_flag; // alive flag will increase 
+	
+	// for PowerOnOff mode
+	unsigned int power_gpio;
+	unsigned int action_time; 
+	unsigned int elapse_time;
+	unsigned int status;
+	unsigned int booting_count; 
+	unsigned int fail_count;
+	unsigned int total_count;
+//time
+};
+
+struct logwrite_t log_writer[6]={0};
+
+unsigned int log_writer_get_alive_count(int i)
+{
+	return log_writer[i].alive_flag;
+}
+
+
+void log_writer_alive_reset(int i)
+{
+ 	log_writer[i].alive_flag=0;
+}
+
+int log_writer_get_number_index(char index)
+{
+	return log_writer[index].num;
+}
+
+void log_writer_get_current_file_handle(char index)
+{
+	char filepath_tmp[64];
+//	snprintf(filepath_tmp,64,"%s%d.log",log_writer[index].folder_path,log_writer[index].num);
+	snprintf(filepath_tmp,64,"E:/ch%.1d/%d.log",index+1,log_writer[index].num);
+//printf("%s\n",filepath_tmp);
+	log_writer[index].handle = fopen(filepath_tmp, "w+");
+	if (NULL == log_writer[index].handle)
+		printf("log_writer_get_current_file_handle can not open %s\n",filepath_tmp);
+
+}
+
+void log_writer_get_next_file_handle(char index)
+{
+
+	if(NULL == log_writer[index].handle)
+		printf("non exist handle\n");
+	else
+		fclose(log_writer[index].handle);
+
+
+	log_writer[index].handle =NULL;
+
+	log_writer_get_current_file_handle(index);
+
+	if(NULL == log_writer[index].handle)
+		printf("log_writer_get_next_file_handle is NULL\n");
+	else
+		log_writer[index].num++;
+
+
+}
+
+
+
+
+
+void creat_writer_folder()
+{
+	int i;
+
+	int ret;
+	char tmp[8] = { 0 };
+	for (i = 0; i<5; i++)
+	{
+		snprintf(tmp, 8, "E:/ch%1.d", i + 1);
+		ret = mkdir(tmp, 0700);
+	}
+	return;
+}
+static void* WritingTask(void* arg)
+{
+	//LogFileInit("E:/log.txt")
+
+	printf("WritingTask start\n");
+
+	char wbf[2048];
+	int wrlen;
+	int i;
+	char index;
+	char cmd;
+
+
+
+// 
+	creat_writer_folder();
+
+	while(!WritingQuit)
+	{
+		if ( (mq_receive(extOutQueue, wbf,0, 0) )> 0)
+		{
+			memcpy(&wrlen,wbf,4);
+			index= wbf[4];
+			cmd= wbf[5];
+
+			//DBG("WR %d , %d/%d byte..\r\n",index, wrlen,log_writer[index].byte_counter );
+			
+			/*
+			
+						DBG("wri: %d byte..\r\n", wrlen,wbf[HEADER_SHIFT]);
+						for(i=0;i<wrlen;i++){
+							DBG("0x%x ", wbf[i+HEADER_SHIFT]);
+						}
+							DBG("\n");
+			
+			*/
+				if(LOG_WRITER_CMD_FAIL_BOOT==cmd) //jump to next 
+				{				
+					DBG("WritingTask cmd = LOG_WRITER_CMD_FAIL_BOOT\n");
+
+					log_writer_get_next_file_handle(index);
+				}
+				else if (LOG_WRITER_CMD_OK_BOOT == cmd) //jump to next 
+				{
+				
+					DBG("WritingTask cmd = LOG_WRITER_CMD_OK_BOOT\n");
+					log_writer_get_next_file_handle(index);
+				}				
+				else
+				{
+					if(NULL ==	log_writer[index].handle)
+					{
+						//ERR("file handle not found(ch%d) \n",index);
+						log_writer_get_current_file_handle(index);  //handle is not opened when initail , do reopen				
+					}
+					else
+					{
+				        fwrite(&wbf[HEADER_SHIFT], 1, wrlen, 	log_writer[index].handle);
+						//TODO: error handle if writing is failed
+						
+						log_writer[index].byte_counter+=wrlen;
+				        fflush(log_writer[index].handle);
+						if(log_writer[index].byte_counter > uart[index].fileMaxsize *1024)
+						{
+							DBG("ch %d write finished. \r\n",index);
+							log_writer[index].byte_counter=0;
+							log_writer_get_next_file_handle(index);
+						}
+
+
+					}
+
+				}
+
+		}
+		else
+		{
+
+
+		}
+
+		
+		usleep(10000);
+	}
+
+	for(index=0;index<5;index++)
+	{
+		if(NULL == log_writer[index].handle)
+			printf("non exist handle\n");
+		else
+			fclose(log_writer[index].handle);
+
+
+		log_writer[index].handle =NULL;
+
+	}	
+//	DBG("wri: %d byte finished\r\n", counter);
+}
+
+void header_set(char* buf,int readLen,char channel,char cmd)
+{
+	memcpy(buf,&readLen,4); //BYTE 0-3
+	*(buf+4)= channel;		//BYTE 4					
+	*(buf+5)= cmd;		//BYTE 5
+
+}
+
+
+void get_fail_total_stringcount(int channel,char* buffer)
+{
+	snprintf(buffer, 64, "%d/%d", log_writer[channel - 1].booting_count, log_writer[channel - 1].total_count);
+	
+}
+
+unsigned int get_fail_count(int channel)
+{
+	return log_writer[channel - 1].fail_count;
+}
+
+unsigned int get_elapsed_time_channel(int channel)
+{
+	return log_writer[channel - 1].elapse_time;
+}
+
+
+static void* ReadUartToLogTask(void* arg)
+{
+int i;
+int readLen;
+char index;
+char* pos;
+char cmd=0;
+	printf("ExternalTask start\n");
+
+    while (!extQuit)
+    {
+		//power control if this is in booting mode
+		
+		for(index=0;index<5;index++)
+		{
+		
+			log_writer[index].elapse_time = (SDL_GetTicks() - log_writer[index].action_time)/1000;
+
+			if(LOG_WRITER_MODE_POWER_ONOFF == log_writer[index].mode)
+			{
+				if(LOG_WRITER_STATUS_IDLE == log_writer[index].status) //do booting
+				{
+					//power on 			
+					ithGpioSetOut(log_writer[index].power_gpio);
+					ithGpioSet(log_writer[index].power_gpio);
+					
+					log_writer[index].status=LOG_WRITER_STATUS_BOOTING; //booting
+					log_writer[index].action_time = SDL_GetTicks();
+					log_writer[index].alive_flag=0;
+					log_writer[index].booting_count++;
+					if (log_writer[index].booting_count >= log_writer[index].total_count )
+						log_writer[index].status = LOG_WRITER_STATUS_END;
+
+				}
+				else if(LOG_WRITER_STATUS_BOOTING == log_writer[index].status)	
+				{			
+				
+					if (log_writer[index].elapse_time >= 30 )
+					{
+						ithGpioSetOut(log_writer[index].power_gpio);
+						ithGpioClear(log_writer[index].power_gpio); //power off 
+
+						//clear rest unread buffer 
+						read(log_writer[index].itp_uart_index , &inDataBuf[HEADER_RESERVED], EXTERNAL_BUFFER_SIZE);
+
+						header_set(inDataBuf,0,index,LOG_WRITER_CMD_FAIL_BOOT);//force to end this file handle
+						mq_send(extOutQueue, inDataBuf,HEADER_SHIFT, 0);
+						
+						log_writer[index].fail_count++;
+						log_writer[index].status=LOG_WRITER_STATUS_POWER_OFF; //booting
+						log_writer[index].action_time = SDL_GetTicks();
+					}
+				}
+				else  if(LOG_WRITER_STATUS_POWER_OFF == log_writer[index].status)
+				{
+					//power off time 
+					if (log_writer[index].elapse_time >= 3 )
+					{
+						log_writer[index].status=LOG_WRITER_STATUS_IDLE; //exceed the resting time do booting
+					}
+
+				}
+				else  if (LOG_WRITER_STATUS_END == log_writer[index].status)
+				{
+					continue;
+				}
+				
+				else
+				{
+
+					ithPrintf("Unknow log wiriter state [0x%x]",log_writer[index].status);
+				}
+
+			}
+
+//		}
+			//for log caputure.
+//		for(index=0;index<5;index++)
+//		{
+				memset(inDataBuf, 0, EXTERNAL_BUFFER_SIZE); 
+				// Read data from UART port
+				
+				readLen = read(	log_writer[index].itp_uart_index , &inDataBuf[HEADER_RESERVED], EXTERNAL_BUFFER_SIZE);
+				/*	
+		
+				if(readLen > 0)
+				{
+				
+					DBG("UART RD: ch %d: %d byte(%d)..(%x,%x)\r\n",index, readLen,readLen,log_writer[index].itp_uart_index,ITP_DEVICE_UART1);
+					for(i=0;i<readLen;i++){
+					printf("0x%x ", inDataBuf[i+HEADER_RESERVED]);
+					}
+					printf("\n");
+
+					continue;
+				}
+				*/			
+
+				if(readLen > 0)
+				{
+					log_writer[index].alive_flag++; // uart aive
+					
+						//check if one second is passsed->record the log
+						if (SDL_GetTicks() - log_writer[index].last_time >= 1000)
+						{
+
+							if(uart[index].timestamp)
+							{					
+								unsigned int min, hour, sec;
+								unsigned int total;
+								total = get_elapsed_total_seconds();
+								min = get_min_passed(total);
+								hour = get_hour_passed(total);
+								sec = get_sec_passed(total);
+							
+								snprintf(&inDataBuf[HEADER_RESERVED-LOG_TIMESTAMP_LEN],LOG_TIMESTAMP_LEN,"\n[%.2d:%.2d:%.2d]\n",hour,min,sec);
+								inDataBuf[HEADER_RESERVED-1]=0xa;//force to change line
+//								printf("TIME= %s",&inDataBuf[HEADER_SHIFT];
+								pos=&inDataBuf[HEADER_RESERVED-HEADER_SHIFT-LOG_TIMESTAMP_LEN];
+
+								readLen+=LOG_TIMESTAMP_LEN;
+
+								log_writer[index].last_time=SDL_GetTicks();
+							}
+							else
+							{
+								pos=&inDataBuf[HEADER_RESERVED-HEADER_SHIFT];
+
+							}
+							
+						}
+						else
+						{
+							pos=&inDataBuf[HEADER_RESERVED-HEADER_SHIFT];
+						}
+					/*
+					memcpy(pos,&readLen,4); //BYTE 0-3
+					*(pos+4)= index;		//BYTE 4					
+					*(pos+5)= cmd;		//BYTE 5
+					*/
+
+					header_set(pos,readLen,index,cmd);
+
+					DBG("read ch %d: %d byte(%d)..\r\n",index, readLen,readLen);
+						
+					 mq_send(extOutQueue, pos,readLen, 0);
+
+					 if (LOG_WRITER_MODE_POWER_ONOFF == log_writer[index].mode)
+					 {
+						 //searching content
+						 if (NULL != strstr(&inDataBuf[HEADER_RESERVED], "[Main]AudioPlay"))
+						 {
+							 printf("BOOTING OK~	\n");
+							 ithGpioSetOut(log_writer[index].power_gpio);
+							 ithGpioClear(log_writer[index].power_gpio); //power off 
+							 log_writer[index].status = LOG_WRITER_STATUS_POWER_OFF; //booting
+							 log_writer[index].action_time = SDL_GetTicks();
+							 header_set(inDataBuf, 0, index, LOG_WRITER_CMD_OK_BOOT);//force to end this file handle
+							 mq_send(extOutQueue, inDataBuf, HEADER_SHIFT, 0);
+						 }
+					 }
+
+				}
+		}
+        usleep(5000);
+    }
+
+/*
+    mq_close(extInQueue);
+	mq_close(extOutQueue);
+    extInQueue = -1;
+	extOutQueue = -1;
+*/
+    return NULL;
+}
+
+
+
+
+void init_log_writer()
+{
+	int index;
+	
+	
+	log_writer[0].itp_uart_index=ITP_DEVICE_UART1;
+	log_writer[1].itp_uart_index=ITP_DEVICE_UART2;
+	log_writer[2].itp_uart_index=ITP_DEVICE_UART3;
+	log_writer[3].itp_uart_index=ITP_DEVICE_UART4;
+	log_writer[4].itp_uart_index=ITP_DEVICE_UART5;
+
+
+
+	log_writer[0].power_gpio=27;
+	log_writer[1].power_gpio=28;
+	log_writer[2].power_gpio=29;
+	log_writer[3].power_gpio=30;
+	log_writer[4].power_gpio=31;
+
+
+	for(index=0;index<5;index++)
+	{
+	
+		log_writer[index].num=1;
+
+		log_writer[index].last_time=SDL_GetTicks();
+		if(NULL != log_writer[index].handle)
+		{
+			fclose(log_writer[index].handle);
+		}
+
+
+		log_writer[index].alive_flag	= 0;
+		log_writer[index].byte_counter	= 0;		
+		log_writer[index].num			= 0;	
+		log_writer[index].fail_count	= 0;
+		log_writer[index].booting_count = 0;
+		log_writer[index].total_count = 100;
+
+	}
+	
+
+	
+
+}
+
+
+
+
+
+void log_writer_normal_mode()
+{
+int index;
+	for(index=0;index<5;index++)
+	{
+		 log_writer[index].mode = LOG_WRITER_MODE_NORMAL;
+	}
+
+}
+void log_writer_poweron_mode()
+{
+int index;
+	for(index=0;index<5;index++)
+	{
+		 log_writer[index].mode = LOG_WRITER_MODE_POWER_ONOFF;
+	}
+
+}
+
+
+void log_writer_stop()
+{
+printf("log_writer_stop \n");
+   extQuit = true;
+   WritingQuit=true;
+   pthread_join(extTask, NULL);
+   pthread_join(WrTask, NULL);
+
+
+}
+
+void log_writer_start()
+{
+
+		init_log_writer();
+
+		set_timecounter_start();//start to cout the elapsed time
+
+
+	    pthread_create(&extTask, NULL, ReadUartToLogTask, NULL);
+
+	    pthread_create(&WrTask, NULL, WritingTask, NULL);
+
+}
+void ExternalInit(void)
+{
+    struct mq_attr qattr;
+
+    qattr.mq_flags = 0;
+    qattr.mq_maxmsg = EXT_MAX_QUEUE_SIZE;
+    qattr.mq_msgsize = sizeof(ExternalEvent);
+
+    extInQueue = mq_open("external_in", O_CREAT | O_NONBLOCK, 0644, &qattr);
+    assert(extInQueue != -1);
+
+    extOutQueue = mq_open("external_out", O_CREAT | O_NONBLOCK, 0644, &qattr);
+    assert(extOutQueue != -1);
+
+    extQuit = false;
+	WritingQuit = false;
+
+#if defined(CFG_UART0_ENABLE) && !defined(CFG_DBG_UART0)
+#ifdef _WIN32
+	if(ComInit(WIN32_COM, CFG_UART0_BAUDRATE)) {
+		//ComInit Error
+		mq_close(extInQueue);
+		mq_close(extOutQueue);
+		extInQueue = -1;
+		extOutQueue = -1;
+		return;
+	}
+#else
+	itpRegisterDevice(ITP_DEVICE_UART0, &itpDeviceUart0);
+    ioctl(ITP_DEVICE_UART0, ITP_IOCTL_INIT, NULL);
+#endif
+#endif
+
+
+
+
+
+}
+
+void ExternalExit(void)
+{
+    extQuit = true;
+    pthread_join(extTask, NULL);
+}
+
+int ExternalReceive(ExternalEvent* ev)
+{
+    assert(ev);
+
+    if (extQuit)
+        return 0;
+
+    if (mq_receive(extInQueue, (char*)ev, sizeof(ExternalEvent), 0) > 0)
+        return 1;
+
+    return 0;
+}
+
+int ExternalSend(ExternalEvent* ev)
+{
+    assert(ev);
+
+    if (extQuit)
+        return -1;
+
+    return mq_send(extOutQueue, (char*)ev, sizeof(ExternalEvent), 0);
+}
